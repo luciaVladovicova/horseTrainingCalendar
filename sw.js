ece2cc90bbde5f771bb892e1fc37b26eecde44e3
luciaVladovicova <57988851+luciaVladovicova@users.noreply.github.com>
2026-09-19 12:42:54 +0200

    add filter

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="jazdecky-kalendar-v5",FILES=["./","./index.html","./script.js","./manifest.json","./icon-192.png","./icon-512.png","./Noto_Sans/static/NotoSans-Regular.ttf"];
+const CACHE="jazdecky-kalendar-v6",FILES=["./","./index.html","./script.js","./manifest.json","./icon-192.png","./icon-512.png","./Noto_Sans/static/NotoSans-Regular.ttf"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{if(e.request.method!=="GET"||new URL(e.request.url).origin!==location.origin)return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match("./index.html"))))});
